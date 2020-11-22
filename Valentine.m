8eb3f38b47821d5e218454f0ba82c3f02ffe5f77
Jose Ignacio <50303036+jgonzet@users.noreply.github.com>
2020-11-22 14:52:43 -0300

    Update Valentine.m

--- Valentine.m
+++ Valentine.m
@@ -1,8 +1,11 @@
 clear all, close all, more off, clc;
 
-nu=3;
-image1 = rgb2gray(imread('test1.jpg')); %Si es .jpg hay que pasar a gris
+% nu is the main hyperparameter of MS algorithm
+nu = 3;
+
+image1 = rgb2gray(imread('test1.jpg')); %convert to gray scale
 %image1 = imread('test1.png');
+
 dim = size(image1);
 regions = defaultRegions(dim);
 numRegions = max(unique(regions));
@@ -11,15 +14,9 @@ for r=0:numRegions
     regions=mergeRegions(r, regions, image1, nu);
 end
 
-%regions=consolidateRegions(regions); % no se usa esta función
+%regions=consolidateRegions(regions); % no se usa esta funciÃ³n
 
-img=double(image1);
-f=getF(regions, image1);
-b=getBorder(regions)*255;
+img = double(image1);
+f = getF(regions, image1);
+b = getBorder(regions)*255;
 imwrite(b,'salida.png','png');
-
-
-
-
-
-
